79a914963c64787e47467be757e2e7fec1ab9133
Benoit Marchant <marchant@mac.com>
2024-09-09 15:32:44 -0700

    comment out debug log

--- data/service/fetch-resource-data-mapping.js
+++ data/service/fetch-resource-data-mapping.js
@@ -179,7 +179,7 @@ exports.FetchResourceDataMapping = class FetchResourceDataMapping extends Expres
                         throw new Error("mapDataOperationToFetchRequests: no url found for dataOperation: ",+dataOperation, " and criteria: "+iCriteria);
                     } else {
                         iRequest = new Request(iUrl, options);
-                        console.debug("Request "+iUrl+" with  options: "+ JSON.stringify(options));
+                        //console.debug("Request "+iUrl+" with  options: "+ JSON.stringify(options));
                         (fetchRequests || (fetchRequests = [])).push(iRequest);
                     }
 
