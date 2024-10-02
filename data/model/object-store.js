1fb7247c62acfbe5d03a426fddf1feab1403bc00
Benoit Marchant <marchant@mac.com>
2024-10-02 15:42:48 -0700

    add ObjectStore to model storage for objects desscribed by ObjectDescriptors

---
+++ data/model/object-store.js
@@ -0,0 +1,31 @@
+/**
+    @module mod/data/model/model/object-store
+*/
+
+var DataObject = require("./data-object").DataObject;
+
+/**
+ * @class ObjectStore
+ * @extends DataObject
+ *
+ * An ObjectStore models the representation of a place where object instances described by an ObjectDescriptor
+ * can be persisted: think of a Table in SQL Database, an IDBObjectStore in WebStandard's IndexeDB or a MangoDB Collection
+ */ 
+
+exports.ObjectStore = class ObjectStore extends DataObject {
+
+    static {
+
+        Montage.defineProperties(this.prototype, {
+            
+            /**
+             * The ObjectStore's name 
+             *
+             * @property {string}
+             * @default null
+             */
+            name: { value: undefined}
+
+        });
+    }
+}
